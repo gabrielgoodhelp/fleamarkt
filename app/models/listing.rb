3b2cabaa115ecd16f2384d893ae88d32da02e4de
Gabriele <gabriele.buonaiuto@gmail.com>
2014-07-25 17:57:58 +0200

    Configured Dropbox in .gitignore and Listing Model

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,7 +1,12 @@
 class Listing < ActiveRecord::Base
-	has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100"}, :default_url => "no_image_available.png",
+	if Rails.env.development?
+		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100"}, :default_url => "no_image_available.png"
+		validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+	else
+		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100"}, :default_url => "no_image_available.png",
 						    :storage => :dropbox,
     						:dropbox_credentials => Rails.root.join("config/dropbox.yml")
-	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
-
+							:path => ":style/:id_:filename"
+		validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+	end
 end

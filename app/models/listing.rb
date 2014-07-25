2eee247c3aebc297a6b0902da2e43c0ce83230b1
Gabriele <gabriele.buonaiuto@gmail.com>
2014-07-25 18:45:27 +0200

    Added Devise, Some Bootstrap Formatting and Alarm features, plus devise_helper in app/helpers/devise

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,7 +5,7 @@ class Listing < ActiveRecord::Base
 	else
 		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100"}, :default_url => "no_image_available.png",
 						    :storage => :dropbox,
-    						:dropbox_credentials => Rails.root.join("config/dropbox.yml")
+    						:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
 							:path => ":style/:id_:filename"
 		validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 	end

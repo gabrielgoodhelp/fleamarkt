12c0332ce7223ffaae36942dd3159ce4fa02537d
Gabriele <gabriele.buonaiuto@gmail.com>
2014-07-25 17:39:07 +0200

    Added Dropbox storage and Figaro Gem

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,7 @@
 class Listing < ActiveRecord::Base
-	has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100"}, :default_url => "no_image_available.png"
+	has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100"}, :default_url => "no_image_available.png",
+						    :storage => :dropbox,
+    						:dropbox_credentials => Rails.root.join("config/dropbox.yml")
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
 end
